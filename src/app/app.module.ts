2288432a73bc9db998ee4b99213749e07331c2a1
casparsylwan <casparsylwan@gmail.com>
2020-04-17 21:20:52 +0200

    pages and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FrontComponent } from './pages/parent/front/front.component';
+import { TopnavComponent } from './pages/components/topnav/topnav.component';
+import { WelcomeComponent } from './pages/parent/welcome/welcome.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FrontComponent,
+    TopnavComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
