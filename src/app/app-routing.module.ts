d7668cf7de66b02a1a83a9d9161f86f4ed7f65be
casparsylwan <casparsylwan@gmail.com>
2020-04-19 23:45:21 +0200

    topnav with animations

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { WelcomeComponent } from './pages/parent/welcome/welcome.component';
 import { FrontComponent } from './pages/parent/front/front.component';
+import { TopnavComponent } from './pages/components/topnav/topnav.component';
 
 
 const routes: Routes = [
   {path:'', component: WelcomeComponent},
-  {path:'order', component: FrontComponent}
+  {path:'order', component: FrontComponent},
+  {path:'top', component: TopnavComponent}
 ];
 
 @NgModule({
--- src/app/pages/components/topnav/topnav.component.ts
+++ src/app/pages/components/topnav/topnav.component.ts
@@ -7,9 +7,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TopnavComponent implements OnInit {
 
+  open:string = 'line';
+  link:string = 'link';
+  box:string  = 'box-nav';
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  public openMenu(){
+
+    if(this.open === 'line close'){
+      this.open = 'line open';
+      this.link = 'link open';
+      this.box =  'box-nav open'
+    }else if(this.open== 'line open'){
+      this.open = 'line close';
+      this.link = 'link close';
+      this.box  = 'box-nav close';
+    }else{
+      this.open = 'line open';
+      this.link = 'link open';
+      this.box =  'box-nav open';
+      
+    } 
   }
 
 }
