677e88e3e8ed6114c309dd1b64316026d69faf52
casparsylwan <casparsylwan@gmail.com>
2020-04-22 10:07:31 +0200

    test

--- src/app/pages/components/movie-selection/movie-selection.component.ts
+++ src/app/pages/components/movie-selection/movie-selection.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+//Models
 import { movies } from '../../../models/movies';
 
 @Component({
@@ -10,17 +12,59 @@ export class MovieSelectionComponent implements OnInit {
 
   //Movie variables
   movieInfo:movies = new movies();
+  urlSafe: SafeResourceUrl;
+  movieName:string="";
+  movieDesc:string ="";
+  ticketprice:number;
+  tickets: number = 1;
+  showTmes:string[];
+  movieDate:string =  "2020-04-25";
+  time:string = "";
+  
+  //State variables
+  state:number = 0;
+  
+  //ClassNames
+  
+  top:string ="top";
+  left:string = "left";
+  right:string = "right";
+  text:string = "text";
+  
+  
+  
+  
+  
 
   //Tickets
   numberOfTicketChoosen:boolean = false;
 
-  constructor() { }
+  
+
+  constructor(private sanitizer: DomSanitizer) { }
 
   ngOnInit(): void {
   }
 
   public showTrailer(index:number){
     console.log("todo");
+    //State change
+    this.state = 1;
+    this.left  = "left video"
+    this.right = "right video"
+    this.text = "text video"
+
+    this.urlSafe = this.sanitizer.bypassSecurityTrustResourceUrl( this.movieInfo.movies[index].trailer);
+    
+    this.movieName =  this.movieInfo.movies[index].name;
+    this.ticketprice = this.movieInfo.movies[index].price;
+    this.showTmes = this.movieInfo.movies[index].date;
+    this.movieDesc = this.movieInfo.movies[index].desc;
+  //  this.reload = !this.reload;
+
+
+
   }
+
 
 }
